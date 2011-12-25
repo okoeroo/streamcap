36f98122755f282445a9a987e1b130f9b2a7d701
Oscar Koeroo <okoeroo@gmail.com>
2011-12-25 14:54:15 +0100

    Added streamdaemon

---
+++ streamdaemon.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+function streamcapture() {
+    URL="$1"
+    NAME="$2"
+    EXT="$3"
+    WATERMARK="$4"
+
+    while [ true ]; do
+        streamcap -url ${URL} -name ${NAME} -extention ${EXT} -highwatermark ${WATERMARK}
+    done
+}
+
+streamcapture "http://194.171.96.102:8000/room2" "room1" "ogv" "100" &
+streamcapture "http://194.171.96.102:8000/room2" "room2" "ogv" "100" &
+
+
