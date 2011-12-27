f72354a19884020a7eec9dcc579867b851848153
Oscar Koeroo <okoeroo@gmail.com>
2011-12-27 12:37:57 +0100

    added recorder files

---
+++ record_saal2.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+HTML_ROOT="/media/ccc/www/html"
+REL_VIDEO_DIR="video"
+ABS_VIDEO_DIR="${HTML_ROOT}/${REL_VIDEO_DIR}"
+
+STREAMCAP="/home/okoeroo/dvl/clib/streamcap/streamcap"
+
+function streamcapture() {
+    URL="$1"
+    NAME="$2"
+    EXT="$3"
+    WATERMARK="$4"
+
+    while [ true ]; do
+        ${STREAMCAP} -url ${URL} -name ${NAME} -extention ${EXT} -highwatermark ${WATERMARK}
+    done
+}
+
+
+# saal1.theora.28c3.fem-net.de
+# saal2.theora.28c3.fem-net.de
+# saal3.theora.28c3.fem-net.de
+
+
+cd "${ABS_VIDEO_DIR}"
+
+#streamcapture "http://194.171.96.102:8000/room2" "room1" "ogv" "100" &
+
+# saal 2
+streamcapture "http://http.28c3.fem-net.de/live/saal1_hq-2011.12.27-11_48_25.ts" "saal2" "ogv" "400M"
+
+
+
+
