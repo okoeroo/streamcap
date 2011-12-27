7c8091c8398ba8500af7de932f68fb683616777e
Oscar Koeroo <okoeroo@gmail.com>
2011-12-27 13:50:57 +0100

    Fixed recorder scripts

--- record_saal1.sh
+++ record_saal1.sh
@@ -6,28 +6,34 @@ ABS_VIDEO_DIR="${HTML_ROOT}/${REL_VIDEO_DIR}"
 
 STREAMCAP="/home/okoeroo/dvl/clib/streamcap/streamcap"
 
+NAME="saal1"
+MAINURL="http://http.28c3.fem-net.de/live/${NAME}_hq.m3u8"
+EXT="ogv"
+WATERMARK="400M"
+
+
 function streamcapture() {
     URL="$1"
-    NAME="$2"
-    EXT="$3"
-    WATERMARK="$4"
+    NAME="$1"
+    EXT="$2"
+    WATERMARK="$3"
 
     while [ true ]; do
+        rand_num=$RANDOM
+        count=0
+        let "rand_num %= 8"
+
+        echo $rand_num
+
+        URL=`curl ${MAINURL} | grep -E "^http" | awk "NR==${rand_num}{print \$1}"`
+
+        echo "Downloading from: $URL"
+
         ${STREAMCAP} -url ${URL} -name ${NAME} -extention ${EXT} -highwatermark ${WATERMARK}
     done
 }
 
-
-# saal1.theora.28c3.fem-net.de
-# saal2.theora.28c3.fem-net.de
-# saal3.theora.28c3.fem-net.de
-
-
-cd "${ABS_VIDEO_DIR}"
-
-#streamcapture "http://194.171.96.102:8000/room2" "room1" "ogv" "100" &
-
-# saal 1
-streamcapture "http://datatomb.de:8080" "saal1" "mms" "400M"
+# main
+streamcapture
 
 
